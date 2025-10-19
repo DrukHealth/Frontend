9265d3cda782f69ee16e208fe44062fee46f775c
Sherab Tshomo <12220065.gcit@rub.edu.bt>
2025-10-19 21:32:32 +0600

    dark and bright mode set up

--- src/pages/userpages/Result.jsx
+++ src/pages/userpages/Result.jsx
@@ -59,6 +59,50 @@ export default function Result() {
         </div>
 
         <div className="nav-title">CTG Diagnosis Result</div>
+
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <label
+            style={{
+              position: "relative",
+              display: "inline-block",
+              width: "50px",
+              height: "26px",
+            }}
+          >
+            <input
+              type="checkbox"
+              checked={darkMode}
+              onChange={() => setDarkMode(!darkMode)}
+              style={{ opacity: 0, width: 0, height: 0 }}
+            />
+            <span
+              style={{
+                position: "absolute",
+                cursor: "pointer",
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                backgroundColor: darkMode ? "#444" : "#ccc",
+                transition: "0.4s",
+                borderRadius: "34px",
+              }}
+            >
+              <span
+                style={{
+                  position: "absolute",
+                  height: "18px",
+                  width: "18px",
+                  left: darkMode ? "26px" : "4px",
+                  bottom: "4px",
+                  backgroundColor: "white",
+                  transition: "0.4s",
+                  borderRadius: "50%",
+                }}
+              ></span>
+            </span>
+          </label>
+        </div>
       </nav>
 
       {/* Main Body */}
